910d41476004b5ef0e968c0331f6c42a26b86ef0
ttnffnfag <ttnffnfagq@gmail.com>
2026-05-19 11:14:01 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,23 @@
-self.addEventListener("install", e=>{
-e.waitUntil(
-caches.open("fa-cache").then(cache=>{
-return cache.addAll([
-"index.html",
-"manifest.json",
-"icon.png"
-]);
-})
-);
+const CACHE_NAME = "fa-app-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", e=>{
-e.respondWith(
-caches.match(e.request).then(r=>{
-return r || fetch(e.request);
-})
-);
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
