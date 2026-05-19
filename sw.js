44e4daea6bcd6bc26ef88b5b813406b8750121d0
ttnffnfag <ttnffnfagq@gmail.com>
2026-05-19 12:51:26 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,18 @@
-const CACHE_NAME = "fa-app-auto-v1";
+const CACHE_NAME = "fa-report-v2";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon.png"
-];
-
-// 安裝：立即接管
-self.addEventListener("install", event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", e=>{
+self.skipWaiting();
 });
 
-// 啟用：刪舊 cache
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener("activate", e=>{
+e.waitUntil(
+caches.keys().then(keys=>{
+return Promise.all(keys.map(k=>caches.delete(k)));
+})
+);
+self.clients.claim();
 });
 
-// 網路優先（避免舊版）
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+self.addEventListener("fetch", e=>{
+e.respondWith(fetch(e.request));
 });
